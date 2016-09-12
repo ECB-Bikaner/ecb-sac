1b82aae87948bf082f5fbafb71f1d75c7011ed1b
Rajat <razat249@gmail.com>
2016-09-12 11:58:47 +0530

    firebase integration with club's pages

--- javascripts/controllers/club_info.js
+++ javascripts/controllers/club_info.js
@@ -1,10 +1,15 @@
 (function(){
   var app = angular.module("clubInfo", ['society_info']);
 
-  app.controller('eventCtrl', ['$scope' ,'$http', '$stateParams', function($scope, $http, $stateParams){
-    $http.get('/' + $stateParams.society_id + '/' + $stateParams.club_id).success(function(data){
-      $scope.events = data;
-    });
+  app.controller('eventCtrl', ['$scope' ,'$http', '$stateParams', '$firebaseArray', function($scope, $http, $stateParams, $firebaseArray){
+    // $http.get('/' + $stateParams.society_id + '/' + $stateParams.club_id).success(function(data){
+    //   $scope.events = data;
+    // });
+    var dict = ["electroparichay", "fiducia", "mad", "robotics", "sched", "techclub"]
+    var rootRef = firebase.database().ref().child('sac');
+    var ref = rootRef.child('events');
+    var query = ref.orderByChild("eventClub").equalTo(dict[$stateParams.club_id]);
+    $scope.events = $firebaseArray(query);
   }]);
 
   app.controller("ClubInfoController", ['$scope', 'society_factory', '$stateParams', function($scope, society_factory, $stateParams) {
